1961f5802b1e6b9eeb940fe3d146da431a6ca0e7
lsnnt <tnityanand523@gmail.com>
2026-06-26 16:48:57 +0530

    Getting raw audio data from blackhole and printing it

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use cpal::traits::{DeviceTrait, HostTrait};
-
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use std::sync::mpsc::channel;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let host = cpal::default_host();
 
@@ -11,6 +11,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = device.default_input_config()?;
 
     println!("Using default output config: {:?}", &config);
+    // Some things to mind for blackhole usually gives this output config so we are building upon it
+    // SupportedStreamConfig { channels: 2, sample_rate: 48000, buffer_size: Range { min: 15, max: 4096 }, sample_format: F32 }
+    let streamconfig: cpal::StreamConfig = config.into();
+    let (tx,rx) = channel();
 
-    Ok(())
+    let err_fn = |err| {
+        eprintln!("Stream error: {}", err);
+    };
+
+    let stream = device.build_input_stream(
+        streamconfig,
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            let _ = tx.send(data.to_vec());
+        },
+        err_fn,
+        None,
+    )?;
+    stream.play()?;
+    std::thread::spawn(move || {
+        while let Ok(data) = rx.recv() {
+            println!("Data received: {:?}", data);
+        }
+    });
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
